0c30e2cdbf0db724bd446f5c88a93e0f0a2780fc
biswa4u85 <biswajit@rbtsb.com>
2021-12-07 15:00:12 +0530

    fixed investment plane

--- src/common/Config.js
+++ src/common/Config.js
@@ -1,7 +1,6 @@
 export default {
   // apiBaseUrl: 'https://www.sipfund.com/api',
   apiBaseUrl: 'http://103.252.169.2:8084/api',
-  apiSocketUrl: 'http://moclass.in:3008',
   uploadPath: '/',
   loginToken: 'Basic U2lwRnVuZC1JZDpTaXBGdW5kLVNlY3JldA==',
   groupIds: {
