ab8cbc0e736e4ce4eedb241ac4b2e5378b4c3106
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2021-12-07 12:26:37 +0530

    show single fund details in OwnersChoiceScreen

--- src/screens/HamburgerMenu/OwnerChoice.js
+++ src/screens/HamburgerMenu/OwnerChoice.js
@@ -1,62 +1,58 @@
 import React, { useState, useRef, useEffect, useContext } from "react";
-import {
-    StyleSheet,
-    View,
-    TouchableOpacity,
-    ActivityIndicator,
-    Text,
-} from "react-native";
-import { connect } from 'react-redux'
-import { Styles, Config, Colors } from '../../common'
-import { MySelectPicker } from '../../components'
-import { AntDesign } from 'react-native-vector-icons';
-import { Image, Header } from 'react-native-elements';
+import { StyleSheet, View, TouchableOpacity, ActivityIndicator, Text } from "react-native";
+import { connect } from "react-redux";
+import { Styles, Config, Colors } from "../../common";
+import { MySelectPicker } from "../../components";
+import { AntDesign } from "react-native-vector-icons";
+import { Image, Header } from "react-native-elements";
 import { ScrollView } from "react-native-gesture-handler";
 
 function OwnerChoice(props) {
     const pageActive = useRef(false);
-    const { token, isFetching, mainCategory, mainCat, subCatagorys, subCat, fetchScheme, schemeCat, schemeGo, choices } = props
+    const { token, isFetching, mainCategory, mainCat, subCatagorys, subCat, fetchScheme, schemeCat, schemeGo, choices } = props;
     const [catList, setCatList] = useState([]);
     const [subcatList, setSubCatList] = useState([]);
     const [schemeList, setSchemeList] = useState([]);
+    const [dataAvailable, setDataAvailable] = useState(false);
     useEffect(() => {
         if (token) {
-            mainCategory(token)
+            mainCategory(token);
         }
     }, [token]);
 
     useEffect(() => {
         if (mainCat) {
-            const stateList = mainCat.map((item) => ({ value: item, label: String(item) }))
-            setCatList(stateList)
+            const stateList = mainCat.map((item) => ({ value: item, label: String(item) }));
+            setCatList(stateList);
         }
     }, [mainCat]);
 
     useEffect(() => {
         if (subCat) {
-            const subcatList = subCat.map((item) => ({ value: item, label: String(item) }))
-            setSubCatList(subcatList)
+            const subcatList = subCat.map((item) => ({ value: item, label: String(item) }));
+            setSubCatList(subcatList);
         }
     }, [subCat]);
 
     useEffect(() => {
         if (schemeCat) {
-            const schemeList = schemeCat.map((item) => ({ value: item.productISIN, label: String(item.productName) }))
-            setSchemeList(schemeList)
+            const schemeList = schemeCat.map((item) => ({ value: item.productISIN, label: String(item.productName) }));
+            setSchemeList(schemeList);
         }
     }, [schemeCat]);
 
     useEffect(() => {
         if (choices && pageActive.current) {
             pageActive.current = false;
-            props.navigation.navigate('TopRatedHome')
+            //props.navigation.navigate('TopRatedHome')
+            setDataAvailable(true);
         }
     }, [choices]);
 
     const [state, setState] = useState({
-        catagory: '',
-        subcatagory: '',
-        scheme: '',
+        catagory: "",
+        subcatagory: "",
+        scheme: "",
     });
 
     const [errors, setErrors] = useState({
@@ -67,40 +63,47 @@ function OwnerChoice(props) {
 
     const catAction = async (catagory) => {
         if (catagory) {
-            setState({ ...state, catagory })
-            subCatagorys({ catagory }, token)
+            setState({ ...state, catagory });
+            subCatagorys({ catagory }, token);
         }
-    }
+    };
 
     const subcatAction = async (subcatagory) => {
         if (subcatagory) {
-            setState({ ...state, subcatagory })
-            fetchScheme({ subcatagory }, token)
+            setState({ ...state, subcatagory });
+            fetchScheme({ subcatagory }, token);
         }
-    }
+    };
 
     const schemeAction = async () => {
         if (state.scheme) {
             pageActive.current = true;
-            schemeGo({ "isin": state.scheme }, token)
+            schemeGo({ isin: state.scheme }, token);
         }
-    }
+    };
 
     return (
         <View style={styles.container}>
             <Header
-                leftComponent={<TouchableOpacity onPress={() => props.navigation.goBack()} style={{ marginTop: 20 }}><AntDesign name={"arrowleft"} size={40} color={Colors.RED} /></TouchableOpacity>}
+                leftComponent={
+                    <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ marginTop: 20 }}>
+                        <AntDesign name={"arrowleft"} size={40} color={Colors.RED} />
+                    </TouchableOpacity>
+                }
                 containerStyle={Styles.header}
                 backgroundColor={Colors.LIGHT_WHITE}
-                centerComponent={<Image
-                    source={require('../../../assets/icon.png')}
-                    style={styles.logimg}
-                />}
-                rightComponent={<View style={{ marginTop: 20, marginRight: 10, }}><AntDesign name={"shoppingcart"} size={40} color={Colors.RED} /></View>}
+                centerComponent={<Image source={require("../../../assets/icon.png")} style={styles.logimg} />}
+                rightComponent={
+                    <View style={{ marginTop: 20, marginRight: 10 }}>
+                        <AntDesign name={"shoppingcart"} size={40} color={Colors.RED} />
+                    </View>
+                }
             />
-            {isFetching && (<View style={Styles.loading}>
-                <ActivityIndicator color={Colors.BLACK} size='large' />
-            </View>)}
+            {isFetching && (
+                <View style={Styles.loading}>
+                    <ActivityIndicator color={Colors.BLACK} size="large" />
+                </View>
+            )}
             <ScrollView style={styles.containerScroll}>
                 <View style={styles.report_sec}>
                     <View>
@@ -108,17 +111,22 @@ function OwnerChoice(props) {
                             values={catList}
                             defultValue={state.catagory}
                             error={errors.catagory}
-                            onChange={(catagory) => { setErrors({ ...errors, catagory: null }); catAction(catagory) }}
+                            onChange={(catagory) => {
+                                setErrors({ ...errors, catagory: null });
+                                catAction(catagory);
+                            }}
                         />
                     </View>
-
 
                     <View>
                         <MySelectPicker
                             values={subcatList}
                             defultValue={state.subcatagory}
                             error={errors.subcatagory}
-                            onChange={(subcatagory) => { setErrors({ ...errors, subcatagory: null }); subcatAction(subcatagory) }}
+                            onChange={(subcatagory) => {
+                                setErrors({ ...errors, subcatagory: null });
+                                subcatAction(subcatagory);
+                            }}
                         />
                     </View>
 
@@ -127,27 +135,67 @@ function OwnerChoice(props) {
                             values={schemeList}
                             defultValue={state.scheme}
                             error={errors.scheme}
-                            onChange={(scheme) => { setErrors({ ...errors, scheme: null }); setState({ ...state, scheme }) }}
+                            onChange={(scheme) => {
+                                setErrors({ ...errors, scheme: null });
+                                setState({ ...state, scheme });
+                            }}
                         />
-                        <View style={{ position: "absolute", right: 10, marginVertical: 20, flexDirection: "row", alignItems: "center", }}>
-                            <AntDesign name="down" size={20} color="#000000" />
+                        <View style={{ margin: 15, flexDirection: "row-reverse" }}>
                             <TouchableOpacity onPress={() => schemeAction()} style={styles.botton_box}>
                                 <Text style={styles.get_otp}>GO</Text>
                             </TouchableOpacity>
                         </View>
                     </View>
-
                 </View>
-                <View style={styles.category_sec}>
-                    <Text style={styles.category}>First select category.</Text>
-                    <Text style={styles.category}>Then select subcategory.</Text>
-                    <Text style={styles.category}>Then select scheme.</Text>
-                    <Text style={styles.category}>Press GO.</Text>
-                    <Text style={styles.category}>For changing scheme please press the scheme nameagain</Text>
-                </View>
+                {dataAvailable ? (
+                    <View style={styles.axis_asset}>
+                        <View style={styles.company}>
+                            <View>
+                                <Image source={{ uri: choices[0]?.nseProductDetail.imagePath }} style={styles.axisimg} />
+                            </View>
+                            <View style={styles.axiswid}>
+                                <Text style={styles.axis}>{choices[0]?.nseProductDetail?.productName}</Text>
+                                <Text style={styles.axis2}>{choices[0]?.text2}</Text>
+                            </View>
+                            <View>
+                                <TouchableOpacity onPress={() => invest(choices[0]?.nseProductDetail?.imagePath, choices[0]?.nseProductDetail?.amcCode, choices[0]?.nseProductDetail?.amcName, choices[0]?.nseProductDetail?.productCode, choices[0]?.nseProductDetail.productName)} style={styles.botton_box}>
+                                    <Text style={styles.get_otp}>INVEST</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        <View style={styles.value_sec}>
+                            <View style={styles.mininvestment}>
+                                <Text style={styles.min}>Min. Investment</Text>
+                                <Text style={styles.min}>{choices[0]?.nseProductDetail.minimumSIPAmount > 1000 ? 1000 : choices[0]?.nseProductDetail.minimumSIPAmount}</Text>
+                            </View>
+                            <View style={styles.mininvestment}>
+                                <Text style={styles.min}>AUM</Text>
+                                <Text style={styles.min}>{choices[0]?.api["PSRP-TotalMarketValueNet"]}</Text>
+                            </View>
+                            <View style={styles.mininvestment}>
+                                <Text style={styles.min}>Returns</Text>
+                                <Text style={styles.min}>{choices[0]?.nseProductDetail.productCurrentNavValue}</Text>
+                            </View>
+                        </View>
+                        <View
+                            style={{
+                                borderWidth: 1,
+                                borderColor: Colors.GREY_1,
+                                marginTop: 10,
+                            }}
+                        ></View>
+                    </View>
+                ) : (
+                    <View style={styles.category_sec}>
+                        <Text style={styles.category}>First select category.</Text>
+                        <Text style={styles.category}>Then select subcategory.</Text>
+                        <Text style={styles.category}>Then select scheme.</Text>
+                        <Text style={styles.category}>Press GO.</Text>
+                        <Text style={styles.category}>For changing scheme please press the scheme nameagain</Text>
+                    </View>
+                )}
             </ScrollView>
         </View>
-
     );
 }
 
@@ -158,10 +206,9 @@ const styles = StyleSheet.create({
 
     container_sec: {
         margin: 10,
-
     },
     containerScroll: {
-        width: '100%'
+        width: "100%",
     },
     logimg: {
         height: 65,
@@ -186,14 +233,12 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         backgroundColor: Colors.RED,
         marginLeft: 5,
-
     },
     get_otp: {
         color: Colors.WHITE,
-        fontSize: 14,
-        fontWeight: 'bold',
+        fontSize: 18,
+        fontWeight: "bold",
         textAlign: "center",
-        paddingVertical: 5,
     },
     category_sec: {
         alignItems: "center",
@@ -203,6 +248,68 @@ const styles = StyleSheet.create({
         fontSize: 13,
         textAlign: "center",
     },
+    returnsbox: { flexDirection: "row" },
+
+    axis_asset: {
+        marginTop: 60,
+        marginHorizontal: 20,
+        paddingTop: 10,
+        borderTopWidth: 1,
+        borderColor: "gray",
+    },
+    company: {
+        flexDirection: "row",
+    },
+    axis: {
+        marginLeft: 10,
+        fontSize: 15,
+    },
+    axiswid: { width: "68%" },
+    axis2: {
+        marginLeft: 10,
+        fontSize: 12,
+        color: Colors.DEEP_GRAY,
+        paddingTop: 3,
+    },
+    axisimg: {
+        height: 39,
+        width: 39,
+    },
+    botton_box: {
+        width: 80,
+        backgroundColor: Colors.RED,
+        height: 20,
+    },
+    get_otp: {
+        color: Colors.WHITE,
+        fontSize: 10,
+        fontWeight: "bold",
+        textAlign: "center",
+        paddingTop: 4,
+    },
+    value_sec: {
+        flexDirection: "row",
+        marginLeft: 50,
+    },
+    mininvestment: {
+        width: "33%",
+        alignItems: "center",
+    },
+    min: {
+        fontSize: 12,
+    },
+    footer_sec: {
+        flexDirection: "row",
+        marginHorizontal: 40,
+        marginVertical: 30,
+        justifyContent: "space-between",
+    },
+    rupees: {
+        width: 40,
+        height: 37,
+    },
+    rupees_sec: { alignItems: "center" },
+    rupees_text: { fontSize: 12 },
 });
 
 const mapStateToProps = (state) => ({
@@ -213,18 +320,26 @@ const mapStateToProps = (state) => ({
     subCat: state.ownerChoice.subCat,
     schemeCat: state.ownerChoice.schemeCat,
     choices: state.ownerChoice.choices,
-})
+});
 
 const mapDispatchToProps = (stateProps, dispatchProps, ownProps) => {
     const { dispatch } = dispatchProps;
-    const { OwnerChoiceActions } = require('../../store/OwnerChoiceRedux')
+    const { OwnerChoiceActions } = require("../../store/OwnerChoiceRedux");
     return {
         ...stateProps,
         ...ownProps,
-        mainCategory: (token) => { OwnerChoiceActions.mainCategory(dispatch, token) },
-        subCatagorys: (params, token) => { OwnerChoiceActions.subCatagorys(dispatch, params, token) },
-        fetchScheme: (params, token) => { OwnerChoiceActions.fetchScheme(dispatch, params, token) },
-        schemeGo: (params, token) => { OwnerChoiceActions.schemeGo(dispatch, params, token) },
-    }
-}
-export default connect(mapStateToProps, undefined, mapDispatchToProps)(OwnerChoice)
+        mainCategory: (token) => {
+            OwnerChoiceActions.mainCategory(dispatch, token);
+        },
+        subCatagorys: (params, token) => {
+            OwnerChoiceActions.subCatagorys(dispatch, params, token);
+        },
+        fetchScheme: (params, token) => {
+            OwnerChoiceActions.fetchScheme(dispatch, params, token);
+        },
+        schemeGo: (params, token) => {
+            OwnerChoiceActions.schemeGo(dispatch, params, token);
+        },
+    };
+};
+export default connect(mapStateToProps, undefined, mapDispatchToProps)(OwnerChoice);
