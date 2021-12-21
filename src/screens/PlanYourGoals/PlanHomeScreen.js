d06425a362977868932640ef8ec7f5cbe0e6a232
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2021-12-21 16:41:26 +0530

    proper labels for sliders

--- src/screens/PlanYourGoals/PlanHomeScreen.js
+++ src/screens/PlanYourGoals/PlanHomeScreen.js
@@ -128,29 +128,20 @@ function PlanHomeScreen(props) {
           </View>
         </View>
 
-        {/* vijay */}
-        <View style={styles.vijay_sec}>
-          <Text style={styles.child2}>Name of Child (Optional)</Text>
-          <TextInput style={styles.childtext} placeholder={"Name"} onChangeText={(name) => setParams({ ...params, name })} value={params.name} />
-        </View>
-
         <View style={[styles.vijay_sec, styles.vijay]}>
-          <Text style={styles.child2}>
-            Current Cost of Education{"\n"}
-            (Tuition fees, stay etc.)
-          </Text>
+          <Text style={styles.child2}>Amount of down payment required</Text>
           <Text style={styles.childtext}>₹{amount}</Text>
         </View>
         <View style={{ marginHorizontal: 20 }}>{additionalInfo.current_cost_amt_req_max && <MySlider value={Number(amount)} change={(amount) => setAmount(amount.toFixed(0))} min={Number(additionalInfo.current_investment_min)} max={Number(additionalInfo.current_investment_max)} />}</View>
 
         <View style={[styles.vijay_sec, styles.vijay]}>
-          <Text style={styles.child2}>Year when this is required</Text>
+          <Text style={styles.child2}>Time when this is required (years)</Text>
           <Text style={styles.childtext}>{time}Y</Text>
         </View>
         <View style={{ marginHorizontal: 20 }}>{additionalInfo.time_when_req_min && <MySlider value={Number(time)} change={(time) => setTime(time.toFixed(0))} min={Number(additionalInfo.time_when_req_min)} max={Number(additionalInfo.time_when_req_max)} />}</View>
 
         <View style={[styles.vijay_sec, styles.vijay]}>
-          <Text style={styles.child2}>Current Investment Value (If Any)</Text>
+          <Text style={styles.child2}>Current Investment set aside (if any)</Text>
           <Text style={styles.childtext}>₹{investment}</Text>
         </View>
         <View style={{ marginHorizontal: 20 }}>{additionalInfo.current_investment_max && <MySlider value={Number(investment)} change={(investment) => setInvestment(investment.toFixed(0))} min={Number(additionalInfo.current_investment_min)} max={Number(additionalInfo.current_investment_max)} />}</View>
