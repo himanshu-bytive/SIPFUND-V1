92cf0e255dddceacb1b45b8361be7be397c4b59f
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2022-03-17 08:54:26 +0530

    pan entry

--- src/screens/Registration/CompleteDetailsScreen.js
+++ src/screens/Registration/CompleteDetailsScreen.js
@@ -474,7 +474,9 @@ function CompleteDetailsScreen(props) {
             autoCapitalize={"characters"}
             value={state.investorPan}
             error={errors.investorPan}
-            editable={false}
+            editable={
+              state?.investorPan && state.investorPan.length > 9 ? false : true
+            }
             selectTextOnFocus={false}
             onChangeText={(investorPan) => {
               setErrors({ ...errors, investorPan: null });
