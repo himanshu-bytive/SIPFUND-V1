2479c8cf57017355c76eefe4a19c377ff83f5732
shivam <shivam@bytive.in>
2024-09-06 12:39:31 +0530

    success msg changed

--- src/screens/Others/UpiScreen.js
+++ src/screens/Others/UpiScreen.js
@@ -663,7 +663,9 @@ function UpiScreen(props) {
                   }}
                 >
                   <Text style={{ fontSize: 22 }}>
-                    Your Payment is successful
+                    Your Payment is
+                    {PaymentStatus !== undefined &&
+                      PaymentStatus.toLowercase() === "success"}
                   </Text>
                   <View
                     style={{
